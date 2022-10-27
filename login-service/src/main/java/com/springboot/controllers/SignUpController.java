3073c9831ce60bb8c766c2a135c42fdcc097495f
Tung Tran <78336970+tt-tung2612002@users.noreply.github.com>
2022-10-27 23:40:59 +0700

    Update SignUpController.java

--- login-service/src/main/java/com/springboot/controllers/SignUpController.java
+++ login-service/src/main/java/com/springboot/controllers/SignUpController.java
@@ -1,13 +1,16 @@
 package com.springboot.controllers;
 
-import com.springboot.dto.request.AppUserSignUpRequest;
-import com.springboot.entity.AppUser;
-import com.springboot.services.UserService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.springboot.dto.request.AppUserRequestDto;
+import com.springboot.dto.request.AppUserSignUpRequest;
+import com.springboot.dto.response.BaseResponse;
+import com.springboot.services.UserService;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,96 +27,117 @@ public class SignUpController {
         PASSWORD_LACKS_UPPERCASE_CHARACTERS(7),
         PASSWORD_LACKS_DIGITS(8),
         PASSWORD_LACKS_SPECIAL_CHARACTERS(9);
+
         private final int code;
+
         SignUpErrorCode(int code) {
             this.code = code;
         }
     }
 
-
     private final UserService userService;
+
+    private final AppUserController appUserController;
 
     @PostMapping("/signup")
     public ResponseEntity<?> signUp(@RequestBody AppUserSignUpRequest appUser) {
 
-        String password = appUser.getPassword();
-
-        if (!(validatePasswordStrength(password) == 0 )) {
-            return ResponseEntity.badRequest().body("Please enter a stronger password, your " +
-                    "password must contain at least an uppercase letter, an lowercase letter, a " +
-                    "number and a special character");
+        // if passwords not match, return error
+        if (!appUser.getPassword().equals(appUser.getConfirmedPassword())) {
+            return ResponseEntity.badRequest().body(new BaseResponse("0", "Passwords do not " +
+                    "match!", ""));
         }
 
-        AppUser user = new AppUser();
-        user.setUsername(appUser.getUsername());
-        user.setPassword(password);
-
-        return ResponseEntity.ok(userService.saveUser(user));
-
-    }
-
-    public int getNumberOfDigits(String password) {
+        // make a request to /api/users/create
+        AppUserRequestDto appUserRequestDto = new AppUserRequestDto(appUser);
         int numberOfDigits = 0;
+        int numberOfUppercaseChars = 0;
+        int numberOfLowercaseChars = 0;
+        int numberOfSpecialChars = 0;
+
+        String password = appUser.getPassword();
+
         for (int i = 0; i < password.length(); i++) {
             if (Character.isDigit(password.charAt(i))) {
                 numberOfDigits++;
             }
-        }
-        return numberOfDigits;
-    }
-
-    public int getNumberOfUppercaseChars(String password) {
-        int numberOfUppercaseChars = 0;
-        for (int i = 0; i < password.length(); i++) {
             if (Character.isUpperCase(password.charAt(i))) {
                 numberOfUppercaseChars++;
             }
-        }
-        return numberOfUppercaseChars;
-    }
-
-    public int getNumberOfLowercaseChars(String password) {
-        int numberOfLowercaseChars = 0;
-        for (int i = 0; i < password.length(); i++) {
             if (Character.isLowerCase(password.charAt(i))) {
                 numberOfLowercaseChars++;
             }
-        }
-        return numberOfLowercaseChars;
-    }
-
-    public int getNumberOfSpecialChars(String password) {
-        int numberOfSpecialChars = 0;
-        for (int i = 0; i < password.length(); i++) {
             if (!Character.isLetterOrDigit(password.charAt(i))) {
                 numberOfSpecialChars++;
             }
         }
-        return numberOfSpecialChars;
+
+        if (validatePassword(password.length(), numberOfDigits, numberOfUppercaseChars,
+                numberOfLowercaseChars,
+                numberOfSpecialChars) == 0) {
+
+            return appUserController.saveUser(appUserRequestDto);
+        }
+
+        return ResponseEntity.badRequest().body(new BaseResponse("0", "Password is not valid!",
+                ""));
+
     }
 
-    // validate password and return corresponding enum
-    public int validatePasswordStrength(String password) {
-        if (password.length() < 8) { // 1
-            return SignUpErrorCode.PASSWORD_TOO_SHORT.code; //2
-        } 
-        if (password.length() > 32) { // 3
-            return SignUpErrorCode.PASSWORD_TOO_LONG.code; // 4
-        } 
-        if (getNumberOfLowercaseChars(password) < 1) { // 5
-            return SignUpErrorCode.PASSWORD_LACKS_LOWERCASE_CHARACTERS.code; // 6
+    public int validatePassword(String password) {
+        int numberOfDigits = 0;
+        int numberOfUppercaseChars = 0;
+        int numberOfLowercaseChars = 0;
+        int numberOfSpecialChars = 0;
+
+        for (int i = 0; i < password.length(); i++) {
+            if (Character.isDigit(password.charAt(i))) {
+                numberOfDigits++;
+            }
+            if (Character.isUpperCase(password.charAt(i))) {
+                numberOfUppercaseChars++;
+            }
+            if (Character.isLowerCase(password.charAt(i))) {
+                numberOfLowercaseChars++;
+            }
+            if (!Character.isLetterOrDigit(password.charAt(i))) {
+                numberOfSpecialChars++;
+            }
         }
-        if (getNumberOfUppercaseChars(password) < 1) { // 7
-            return SignUpErrorCode.PASSWORD_LACKS_UPPERCASE_CHARACTERS.code; // 8
-        }
-        if (getNumberOfDigits(password) < 1) { // 9
-            return SignUpErrorCode.PASSWORD_LACKS_DIGITS.code; // 10
-        } 
-        if (getNumberOfSpecialChars(password) < 1) { // 11
-            return SignUpErrorCode.PASSWORD_LACKS_SPECIAL_CHARACTERS.code; // 12
-        }
-        return 0; // 13
+
+        return validatePassword(password.length(), numberOfDigits, numberOfUppercaseChars,
+                numberOfLowercaseChars,
+                numberOfSpecialChars);
     }
 
+    public int validatePassword(int passLength, int numberOfDigits, int numberOfUppercaseChars,
+            int numberOfLowercaseChars, int numberOfSpecialChars) {
+
+        if (passLength < 8) {
+            return SignUpErrorCode.PASSWORD_TOO_SHORT.code;
+        }
+
+        if (passLength > 32) {
+            return SignUpErrorCode.PASSWORD_TOO_LONG.code;
+        }
+
+        if (numberOfDigits == 0) {
+            return SignUpErrorCode.PASSWORD_LACKS_DIGITS.code;
+        }
+
+        if (numberOfLowercaseChars == 0) {
+            return SignUpErrorCode.PASSWORD_LACKS_LOWERCASE_CHARACTERS.code;
+        }
+
+        if (numberOfUppercaseChars == 0) {
+            return SignUpErrorCode.PASSWORD_LACKS_UPPERCASE_CHARACTERS.code;
+        }
+
+        if (numberOfSpecialChars == 0) {
+            return SignUpErrorCode.PASSWORD_LACKS_SPECIAL_CHARACTERS.code;
+        }
+
+        return 0;
+    }
 
 }
